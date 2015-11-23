51032ddd001c315e453f68a3fbccd63590c22a32
Eru0 <lalmeida@S33-PC04.EFLI.LOCAL>
2015-11-23 13:30:19 +0000

    Novo
    Nova estrotura de dados

--- carsell.cbl
+++ carsell.cbl
@@ -12,7 +12,15 @@
 	   file section.
 	   fd fic.
 	   01 registo.
-	   		02 data_registo.
+	   		03 data_registo.
 	   			05 dia pic 99.
 	   			05 mes pic 99.
-	   			05 ano pic 9999.
+	   			05 ano pic 9(4).
+	   		03 matricula   pic x(6).
+	   		03 nome        pic a(15).
+	   		03 marca       pic a(15).
+	   		03 modelo      pic x(10).
+	   		03 contacto    pic 9(9).
+	   		03 ano_fabrico pic 9(4).
+
+
