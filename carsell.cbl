c32bb4b0acbc3773a98cc9ac79e8eb78de88ac55
Eru0 <lalmeida@S33-PC04.EFLI.LOCAL>
2015-11-23 13:04:47 +0000

    main program
    firts part

---
+++ carsell.cbl
@@ -0,0 +1,12 @@
+	   identification division.
+	   program-id. CARSELL.
+	   author. Eru&Rafa.
+
+	   environment division.
+	   input-output section.
+	   file-control.
+	   		select optional fic assign "HOME.DAT"
+	   		organization is line sequential.
+
+	   data division.
+	   file section.
