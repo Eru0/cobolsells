09254351092d0bd82168e9a7f0f966b274ae7fc6
Luís Almeida <luiseru123@gmail.com>
2015-11-24 16:50:47 +0000

    Mudanças no carsell, paragrafo dos registos com 1 erro :(

--- carsell.cbl
+++ carsell.cbl
@@ -12,19 +12,20 @@
 	   file section.
 	   fd fic.
 	   01 registo.
-	   		03 data_registo.
-	   			05 dia pic 99.
-	   			05 mes pic 99.
+	   		03 date-registo.
 	   			05 ano pic 9(4).
+	   			05 mes pic 99.
+	   			05 dia pic 99.
 	   		03 matricula   pic x(6).
 	   		03 nome        pic a(15).
 	   		03 marca       pic a(15).
 	   		03 modelo      pic x(10).
 	   		03 contacto    pic 9(9).
-	   		03 ano_fabrico pic 9(4).
+	   		03 ano-fabrico pic 9(4).
 
 	   working-storage section.
 	   77 opcao pic 9.
+	   77 pausa pic x.
 
 	   screen section.
 	   01 cls blank screen.
@@ -32,29 +33,64 @@
 	   procedure division.
 	   ecra.
 	   		perform until opcao = 0
+	   		accept opcao
 	   		evaluate opcao
-	   			when 1 go to registo
+	   			when 1 go to registos
 	   			when 2 go to pesquisa
 	   			when 3 go to escrita
 	   			when 4 go to marcas
 	   			when 5 go to valores
 	   			when 6 go to velhos
-	   			when 7 go to vendas_marca
-	   			when 8 go to total_marca
-	   			when 9 go to maior.
+	   			when 7 go to vendas-marca
+	   			when 8 go to total-marca
+	   			when 9 go to maior
+	   			when 0 go to fim
 	   			when other go to ecra
 	   		end-evaluate
 	  		end-perform.
-	   registo.
+	   registos.
+	   		display cls.
+ 	   		open extend fic.
+	   		accept date-registo from date yyyymmdd.
+	   		display "Nome do clinete: ".
+	   		accept nome.
+	   		display "Contacto: ".
+	   		accept contacto.
+	   		display "Marca do veiculo: ".
+	   		accept marca.
+	   		display "Modelo do veiculo: ".
+	   		accept modelo.
+	   		display "Ano de fabrico: ".
+	   		accept ano-fabrico.
+	   		display "Matricula: ".
+	   		accept matricula.
+
+	   		write registo.
+	   		close fic.
+	   		display cls.
+	   		display "Registo concluido".
+	   		display "Quer fazer outro?(1-Sim | 2-Não): ".
+	   		accept pausa.
+	   		if pausa = 1 then go to registos
+	   		end-if.
+	   		go ecra.
 	   pesquisa.
+	   		go ecra.
 	   escrita.
+	   		go ecra.
 	   marcas.
+	   		go ecra.
 	   valores.
+	   		go ecra.
 	   velhos.
-	   vendas_marca.
-	   total_marca.
+	   		go ecra.
+	   vendas-marca.
+	   		go ecra.
+	   total-marca.
+	   		go ecra.
 	   maior.
+	   		go ecra.
 	   fim.
-	   stop run.
+	   		stop run.
 
 
