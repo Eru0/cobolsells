12f7d642c1eb78d0b1c92c0943e3e2840002383b
Eru0 <lalmeida@S33-PC04.EFLI.LOCAL>
2015-11-25 08:40:42 +0000

    Main Novo

--- carsell.cbl
+++ carsell.cbl
@@ -13,25 +13,41 @@
 	   fd fic.
 	   01 registo.
 	   		03 date-registo.
-	   			05 ano pic 9(4).
+	   			05 ano pic 99.
 	   			05 mes pic 99.
 	   			05 dia pic 99.
-	   		03 matricula   pic x(6).
-	   		03 nome        pic a(15).
+	   		03 nome        pic a(20).
+	   		03 contacto    pic 9(9).
 	   		03 marca       pic a(15).
 	   		03 modelo      pic x(10).
-	   		03 contacto    pic 9(9).
 	   		03 ano-fabrico pic 9(4).
+	   		03 matricula   pic x(6).
+	   		03 km          pic 9(6).
+	   		03 preco       pic 9(6)v99.
 
 	   working-storage section.
 	   77 opcao pic 9.
 	   77 pausa pic x.
+	   77 wmatr pic x(6).
 
 	   screen section.
 	   01 cls blank screen.
 
 	   procedure division.
 	   ecra.
+	   		display cls.
+			display "*** Menu ***".
+			display "1 - Novo Registo".
+			display "2 - Pesquisar P/Matricula".
+			display "3 - Mostrar Tudo".
+			display "4 - Pesquisar P/Marca".
+			display "5 - Listagem Geral C/Iva Incluido".
+			display "6 - Mostrar Viaturas de Fabrico < ao ano 2000".
+			display "7 - Total de Viaturas Vendidas P/Marca".
+			display "8 - Total P/Marca".
+		 	display "9 - Maior Venda".
+			display "0 - Sair".
+			display "Digite a opcao que pretende".
 	   		perform until opcao = 0
 	   		accept opcao
 	   		evaluate opcao
@@ -51,8 +67,8 @@
 	   registos.
 	   		display cls.
  	   		open extend fic.
-	   		accept date-registo from date yyyymmdd.
-	   		display "Nome do clinete: ".
+	   		accept date-registo from date.
+	   		display "Nome do cliente: ".
 	   		accept nome.
 	   		display "Contacto: ".
 	   		accept contacto.
@@ -64,17 +80,32 @@
 	   		accept ano-fabrico.
 	   		display "Matricula: ".
 	   		accept matricula.
+	   		display "Quilometragem atual: "
+	   		accept km.
+	   		display "Preco de Venda: "
+	   		accept preco.
 
 	   		write registo.
 	   		close fic.
 	   		display cls.
 	   		display "Registo concluido".
-	   		display "Quer fazer outro?(1-Sim | 2-Não): ".
+	   		display "Quer fazer outro?(1-Sim | 2-Nao): ".
 	   		accept pausa.
 	   		if pausa = 1 then go to registos
 	   		end-if.
 	   		go ecra.
 	   pesquisa.
+	   		display cls.
+	   		display "Qual a matricula?: ".
+	   		accept wmatr.
+	   		open input fic.
+	   		perform until registo = high-values
+	   			read fic
+	   				at end move high-values to registo
+	   			end-read
+	   			if not registo = high-values
+	   				if matricula = wmatr
+	   				display 
 	   		go ecra.
 	   escrita.
 	   		go ecra.
